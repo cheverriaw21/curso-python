0bcf6a0863fb09a10f1bff00bf2f8352e7eb9b72
William Giovanni Cheverria Pacheco <cheverriawg@gmail.com>
2023-01-02 13:05:42 -0600

    Update tarea2.py

---
+++ tarea002/tarea2.py
@@ -0,0 +1,12 @@
+#Hallar la Potencia
+print("==POTENCIA==")
+
+#Entrada de datos
+a = int(input("Introduzca un numero entero positivo para la Base:"))
+n = int(input("Introduzca un numero entero positivo para el Exponente:"))
+
+#Proceso para encontrar la potencia
+p = a**n
+
+#Mostrar el resultado
+print(f"La potencia es: {p}")
